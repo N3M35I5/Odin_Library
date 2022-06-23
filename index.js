9867b5cfb9841a6719697b3ef5ca0053113dc8f5
N3M35I5 <adwaith1998kv@gmail.com>
2022-06-23 11:06:03 +0530

    added functionality to add and delete books

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-function Book(title,auther,pages,readStatus){
+const content = document.querySelector('.content');
+const card_template = document.createElement("div");
+card_template.classList.add('card');
+
+function Book(title,author,pages,readStatus){
     this.title=title;
-    this.auther=auther;
+    this.author=author;
     this.pages=pages;
     this.readStatus=readStatus;
 };
 Book.prototype.info = function(){
-    str=this.title+" by "+ this.auther+", "+this.pages+"pages, ";
+    str=this.title+" by "+ this.author+", "+this.pages+"pages, ";
     if(this.readStatus)
         str+="read completly";
     else
@@ -17,14 +21,62 @@ let myLibrary = [];
 
 function addBookToLibrary(newBook) {
     myLibrary.push(newBook);
+    displaylib(myLibrary)
 };
 
-const book1 = new Book("a","a",123,true);
-const book2 = new Book("a","a",123,true);
-const book3 = new Book("a","a",123,true);
-const book4 = new Book("a","a",123,true);
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-addBookToLibrary(book4);
-console.log(myLibrary);
+function displaylib(lib){
+    //remove old childred and replace with new ones
+    content.replaceChildren();
+    //adding new children
+    lib.forEach(book => {
+        const card = card_template.cloneNode(true);
+        const title = document.createElement("div");
+        title.textContent="Title: "+book.title;
+        card.appendChild(title);
+
+        const author = document.createElement("div");
+        author.textContent="Author: "+book.author;
+        card.appendChild(author);
+
+        const pages = document.createElement("div");
+        pages.textContent="Pages: "+book.pages;
+        card.appendChild(pages);
+
+        const readStatus = document.createElement("div");
+        if(book.readStatus)
+            readStatus.textContent="Completed";
+        else
+            readStatus.textContent="Not Completed";
+        card.appendChild(readStatus);
+
+        const delbtn=document.createElement("button");
+        delbtn.addEventListener("click",()=>{
+            var filtered = myLibrary.filter(function(value, index, arr){ 
+                return value.title !== book.title;
+            });
+            myLibrary=filtered;
+            displaylib(myLibrary);
+        });
+        delbtn.textContent="Delete"
+        card.appendChild(delbtn);
+
+        content.appendChild(card);
+    });
+};
+
+const book_form= document.querySelector('#book-form');
+book_form.addEventListener('submit',handleSubmit);
+function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+    const title = formProps.title;
+    const author = formProps.author;
+    const pages=formProps.pages;
+    const readbtn = formProps.readbtn;
+    const book = new Book(title,author,pages,readbtn);
+    addBookToLibrary(book);
+};
+
+
+//console.log(myLibrary);
